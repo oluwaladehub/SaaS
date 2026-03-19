04f9b9bdb5666da17abafd81cfbeb8f649eb1d2c
oluwaladehub <oluwaladen@gmail.com>
2026-03-19 22:29:43 +0100

    3D generation

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,27 +1,116 @@
-import {useLocation} from "react-router";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect, useRef, useState} from "react";
+import {generate3DView} from "../../lib/ai.action";
+import {Box, Download, RefreshCcw, Share2, X} from "lucide-react";
+import Button from "../../components/ui/Button";
 
 const VisualizerId = () => {
+    const navigate = useNavigate();
     const location = useLocation();
     const { initialImage, initialRender, name } = location.state || {};
-    return (
-        <section>
-            <h1>{name || 'Untitled Project'}</h1>
 
-            <div className="visualizer">
-                {initialRender && (
-                    <div className="image-container">
-                        <h2>Rendered Image</h2>
-                        <img src={initialRender} alt="Rendered" />
+    const hasInitialGenerated = useRef(false);
+
+    const[currentImage, setCurrentImage] = useState<string | null>(initialRender || null);
+    const [isProcessing, setIsProcessing] = useState(false);
+
+    const handleBack = () => navigate('/');
+
+    const runGeneration = async () => {
+        if(!initialImage) return;
+
+        try {
+            setIsProcessing(true);
+            const result = await generate3DView({ sourceImage: initialImage });
+
+            if(result.renderedImage) {
+                setCurrentImage(result.renderedImage);
+
+            //     update render image
+            }
+        } catch (error) {
+            console.error('Failed to generate 3D view:', error);
+        } finally {
+            setIsProcessing(false);
+        }
+    }
+
+    useEffect(() => {
+        if(!initialImage || hasInitialGenerated.current) return;
+
+        if(initialRender) {
+            setCurrentImage(initialRender);
+            hasInitialGenerated.current = true;
+            return;
+        }
+
+        hasInitialGenerated.current = true;
+        runGeneration()
+    }, [initialImage, initialRender]);
+
+    return (
+        <div className="visualizer">
+            <nav className="topbar">
+                <div className="brand">
+                    <Box className="logo"/>
+
+                    <span className="name">
+                        Roomify
+                    </span>
+                </div>
+                <Button onClick={handleBack} variant="ghost" size="sm" className="exit">
+                    <X className="icon" /> Exit Editor
+                </Button>
+            </nav>
+
+            <section className="content">
+                <div className="panel">
+                    <div className="panel-header">
+                        <div className="panel-meta">
+                            <p>Project</p>
+                            <h2>{'Untitled Project'}</h2>
+                            <p className="note">Created by You</p>
+                        </div>
+
+                        <div className="panel-actions">
+                            <Button
+                                onClick={() => {}}
+                                size="sm" className="export"
+                                disabled={!currentImage}>
+                                <Download className="w-4 h-4 mr-2" /> Export
+                            </Button>
+                            <Button size='sm'
+                                    onClick={() => {}}
+                                    className="share" >
+                                <Share2 className="w-4 h-4 mr-2" /> Share
+                            </Button>
+                        </div>
                     </div>
-                )}
-                {initialImage && (
-                    <div className="image-container">
-                        <h2>Source Image</h2>
-                        <img src={initialImage} alt="Source" />
+
+                    <div className={`render-area ${isProcessing ? 'processing' : ''}`}>
+                        {currentImage ? (
+                            <img src={currentImage} alt="AI Render" className="render-img" />
+                        ) : (
+                            <div className="render-placeholder">
+                                {initialImage && (
+                                    <img src={initialImage} alt="Original" className="render-fallback" />
+                                )}
+                            </div>
+                        )}
+
+                        {isProcessing && (
+                            <div className="render-overlay">
+                                <div className="rendering-card">
+                                    <RefreshCcw className="spinner" />
+                                    <span className="title">Rendering...</span>
+                                    <span className="subtitle">Generating your 3D visualisation</span>
+                                </div>
+                            </div>
+                        )}
                     </div>
-                )}
-            </div>
-        </section>
+                </div>
+            </section>
+        </div>
     )
 }
 export default VisualizerId
